e9c3f620d86d2e4769ad757cec276f9f9fa53715
Lopatina Darya <baboleh@rambler.ru>
2022-04-03 21:19:47 +0300

    Ввели переменную для смены кодировки и записали 2 новых файла 1 - в кодировке utf-8, 2 - в кодировке cp 1251

--- main.py
+++ main.py
@@ -3,20 +3,20 @@ import random
 from time import sleep
 import os
 
-base_path = os.getcwd() #путь, где лежит документ
+base_path = os.getcwd() #путь, где лежит документ (если путь не указывать, файл создастся в той же папке, где лежит сам проект
 print(base_path)
 log_dir = "logs" #название каталога
-file_name = "logs.txt" #название файла(который создастся
+file_name_utf = "log_utf.txt" #название файла(который создастся)
+file_name_cp = "log_cp.txt"
 
-full_path = os.path.join(base_path, log_dir, file_name)
-print(f"=====> {full_path}")
+full_path_utf = os.path.join(base_path, log_dir, file_name_utf)
+full_path_cp = os.path.join(base_path, log_dir, file_name_cp)
 
-def file_worker(file_name: str, mode: str, data: str):
-    with open(file_name, mode) as file:
+
+def file_worker(file_name: str, mode: str, encoding: str, data: str): # ввели переменную для смены кодировки
+    with open(file_name, mode, encoding=encoding) as file:
         file.write(data)
 
-for i in range(10):
-    date_time = datetime.now()
-    log_str = f"{date_time} - {random.randint(1, 1000)} \n"
-    sleep(1)
-    file_worker(full_path, "a", log_str)
+file_worker(full_path_utf, "w", 'utf-8', 'Привет') # создали файл в папке Logs в кодировке utf-8
+file_worker(full_path_cp, "w", 'cp1251', 'Привет') # создали файл в папке Logs в кодировке cp1251
+
